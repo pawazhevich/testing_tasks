522860e53e30f76c31d69ca042e41b61ab17ead1
pazhevich <375292703311p@gmail.com>
2019-10-31 11:30:05 +0300

    implemented method to check if triangle possible

--- Triangle/src/by/bsu/triangle/util/TriangleWorker.java
+++ Triangle/src/by/bsu/triangle/util/TriangleWorker.java
@@ -1,6 +1,15 @@
 package by.bsu.triangle.util;
 
+import by.bsu.triangle.exception.TriangleNegativeSideException;
+
 public class TriangleWorker {
 
+   public static boolean isTrianglePossible (double a, double b, double c) throws TriangleNegativeSideException{
 
+       if (a<0 || b<0 || c<0) {
+            throw new TriangleNegativeSideException("One of the sides is negative");
+        }
+
+       return (a <= b + c) && (b <= a + c) && (c <= a + b);
+   }
 }
