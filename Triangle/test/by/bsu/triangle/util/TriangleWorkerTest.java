719b37c3ec66eb5fb35df81c1d349e272ddf7f7c
pazhevich <375292703311p@gmail.com>
2019-10-31 12:14:01 +0300

    Created test cases for size equal or less then sum

--- Triangle/test/by/bsu/triangle/util/TriangleWorkerTest.java
+++ Triangle/test/by/bsu/triangle/util/TriangleWorkerTest.java
@@ -1,7 +1,29 @@
 package by.bsu.triangle.util;
 
+import by.bsu.triangle.exception.TriangleInvalidSideException;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TriangleWorkerTest {
+
+    @Test
+    public void isTrianglePossibleTestSideEqualSum() throws TriangleInvalidSideException {
+        double a = 4;
+        double b = 5;
+        double c = a + b;
+        assertFalse(TriangleWorker.isTrianglePossible(a,b,c));
+    }
+
+    @Test
+    public void isTrianglePossibleTestSideLessSum() throws TriangleInvalidSideException {
+        double a = 7;
+        double b = 8;
+        double c = a + b - 1;
+        assertFalse(TriangleWorker.isTrianglePossible(c,a,b));
+    }
+
     
+
+
 }
