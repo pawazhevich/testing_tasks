4b9fb44ac1c1201a1c6ebec726810c9d1aa5b887
pazhevich <375292703311p@gmail.com>
2019-10-31 11:59:41 +0300

    reworked for new exception

--- Triangle/src/by/bsu/triangle/util/TriangleWorker.java
+++ Triangle/src/by/bsu/triangle/util/TriangleWorker.java
@@ -1,13 +1,13 @@
 package by.bsu.triangle.util;
 
-import by.bsu.triangle.exception.TriangleNegativeSideException;
+import by.bsu.triangle.exception.TriangleInvalidSideException;
 
 public class TriangleWorker {
 
-   public static boolean isTrianglePossible (double a, double b, double c) throws TriangleNegativeSideException{
+   public static boolean isTrianglePossible (double a, double b, double c) throws TriangleInvalidSideException {
 
-       if (a<0 || b<0 || c<0) {
-            throw new TriangleNegativeSideException("One of the sides is negative");
+       if (a<=0 || b<=0 || c<=0) {
+            throw new TriangleInvalidSideException("Triangle sides must be positive");
         }
 
        return (a <= b + c) && (b <= a + c) && (c <= a + b);
